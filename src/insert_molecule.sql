ffa1ee75b752247b05c6a1ff28d5f362176d8900
Brent Westbrook <brentrwestbrook@gmail.com>
2024-03-25 13:06:50 -0400

    ignore duplicate smiles on insert

--- src/insert_molecule.sql
+++ src/insert_molecule.sql
@@ -1,1 +1,2 @@
-insert into molecules (smiles, moldata) values (?1, ?2)
+-- insert a new molecule if the SMILES is not already present
+insert or ignore into molecules (smiles, moldata) values (?1, ?2)
